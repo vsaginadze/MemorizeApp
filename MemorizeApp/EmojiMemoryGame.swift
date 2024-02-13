34d6fa107fc931b27de6d17fe7b863f119eb240d
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-02-13 20:36:50 +0400

    finished EmojiMemoryGame viewModel

--- MemorizeApp/EmojiMemoryGame.swift
+++ MemorizeApp/EmojiMemoryGame.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class EmojiMemoryGame: ObservableObject {
+    typealias Card = MemoryGame<String>.Card
+    
+    private static let emojis = ["🎃", "👻", "🦇", "🕷️", "🕸️", "🧟", "🧛‍♂️", "🧙‍♀️", "🌕"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame(numberOfPairsOfCards: 8) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published var model = createMemoryGame()
+    
+    var cards: Array<Card> {
+        model.cards
+    }
+    
+    // MARK: Intents
+    
+    func choose(_ card: Card) {
+        model.choose(card)
+    }
+    
+    func shuffle() {
+        model.shuffle()
+    }
+}
