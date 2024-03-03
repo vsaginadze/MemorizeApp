797f61de09bd4f74a3999156ca69b8d06d9b9a4b
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-03 04:44:51 +0400

    modified viewModel to accomodate several themes

--- MemorizeApp/EmojiMemoryGame.swift
+++ MemorizeApp/EmojiMemoryGame.swift
@@ -10,20 +10,27 @@ import Foundation
 class EmojiMemoryGame: ObservableObject {
     typealias Card = MemoryGame<String>.Card
     
-    private static let emojis = ["🎃", "👻", "🦇", "🕷️", "🕸️", "🧟", "🧛‍♂️", "🧙‍♀️", "🌕"]
+    private let numberOfPairsOfCards = 10
+    private let themes = ["Nature", "Food", "Weather", "Animal", "Travel", "Celebration"]
     
-    private static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame(numberOfPairsOfCards: 8) { pairIndex in
-            if emojis.indices.contains(pairIndex) {
-                return emojis[pairIndex]
+    init() {
+        theme = Theme(name: themes.randomElement() ?? "Nature", numberOfPairsOfCards: numberOfPairsOfCards)
+        model = EmojiMemoryGame.createMemoryGame(withTheme: theme)
+        shuffle()
+    }
+    
+    private static func createMemoryGame(withTheme theme: Theme) -> MemoryGame<String> {
+        MemoryGame(numberOfPairsOfCards: theme.numberOfPairsOfCards) { pairIndex in
+            if theme.emojis.indices.contains(pairIndex) {
+                return theme.emojis[pairIndex]
             } else {
                 return "⁉️"
             }
         }
     }
     
-    @Published var model = createMemoryGame()
-    
+    @Published var model: MemoryGame<String>
+    var theme: Theme
     var cards: Array<Card> {
         model.cards
     }
@@ -36,5 +43,13 @@ class EmojiMemoryGame: ObservableObject {
     
     func shuffle() {
         model.shuffle()
+    }
+    
+    func startNewGame() {
+        let themeName = themes.randomElement() ?? "Nature"
+        print(themeName)
+        theme = Theme(name: themeName, numberOfPairsOfCards: numberOfPairsOfCards)
+        model = EmojiMemoryGame.createMemoryGame(withTheme: theme)
+        shuffle()
     }
 }
